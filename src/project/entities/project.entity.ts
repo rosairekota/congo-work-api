11603385e7d0217c0ffbc03f8b37fd8ca244451f
rkota <rosairekota@gmail.com>
2022-01-15 16:37:54 +0100

    feat:add Project entity

--- src/project/entities/project.entity.ts
+++ src/project/entities/project.entity.ts
@@ -1,1 +1,25 @@
-export class Project {}
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+
+@Entity()
+export class Project {
+    
+    @PrimaryKey()
+    id:number
+
+    @Property()
+    title:string
+
+    @Property()
+    description?:string
+
+    @Property({name:"image_url"})
+    imageUrl?:string
+
+    @Property({name:"github_link"})
+    githubLink?:string;
+
+    @Property({name:"website_link"})
+    websiteLink:string;
+
+
+}
