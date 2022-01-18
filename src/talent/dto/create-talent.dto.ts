53246d5064f1e4c4cddb5d7111c3a43b699e4c28
rkota <rosairekota@gmail.com>
2022-01-18 16:44:18 +0100

    feat:add create Talent dto

--- src/talent/dto/create-talent.dto.ts
+++ src/talent/dto/create-talent.dto.ts
@@ -1,1 +1,39 @@
-export class CreateTalentDto {}
+import { Profession } from './../../profession/entities/profession.entity';
+import { UserStatusEnum } from './../../shared/enums/user-status-enum';
+import { IsEnum, IsNotEmpty, IsObject, IsOptional } from 'class-validator';
+import { User } from '../../auth/entities/user.entity';
+export class CreateTalentDto {
+
+  @IsOptional()
+  experience: string;
+
+  @IsNotEmpty()
+  level: string;
+
+  @IsNotEmpty()
+  education?: string;
+
+  @IsNotEmpty()
+  githubLink?: string;
+
+  @IsNotEmpty()
+  websiteLink?: string;
+
+  @IsNotEmpty()
+  location?: string;
+
+  @IsNotEmpty()
+  phone: string;
+
+  @IsOptional()
+  @IsEnum(UserStatusEnum)
+  availability: UserStatusEnum;
+
+  @IsNotEmpty()
+  @IsObject()
+  user: User;
+  
+  @IsNotEmpty()
+  @IsObject()
+  profession: Profession;
+}
